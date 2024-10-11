e763efb243f78510e08a6d03ec5de9b758e8ae26
Shraddha Ranjan <ranjanshraddha@yahoo.co.uk>
2024-10-11 21:27:40 +0100

    Added empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true; 
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset /> 
+      </ClientOnly>
+    )
+  }
+  
   return (
-    <div className="text-xl text-purple-600">
-      Hello RanjanBooks!
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
+        lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div> My future books</div>
+        </div>
+      </Container>
+    </ClientOnly>
+
   );
 }
